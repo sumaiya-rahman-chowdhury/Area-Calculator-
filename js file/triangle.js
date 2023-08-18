9ba9b49e696a86a5981e5e4c91faa214e63bc1c4
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-08-18 01:37:31 -0700

    first commit

---
+++ js file/triangle.js
@@ -0,0 +1,17 @@
+console.log("Lana")
+
+function calculateTriangleArea(){
+    // console.log("clicked")
+    const baseString = document.getElementById('base').value ;
+    // console.log(typeof(baseString))
+    const base = parseFloat(baseString) ;
+    // console.log(typeof(base)) 
+    // type is number now 
+    // console.log(base)
+
+    const heightString = document.getElementById('height').value ;
+    const height = parseFloat(heightString) ;
+    // console.log(base * height * 0.5)
+    // return base * height * 0.5 ;
+    document.getElementById('result-triangle').innerText = base * height * 0.5 ;
+}
